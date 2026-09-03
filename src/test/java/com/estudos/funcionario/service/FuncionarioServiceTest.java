a4392c81f24a12e152be4b04de80132a42ea93eb
Junio Alves Barbosa <junio.barbosa@ovg.org.br>
2026-09-03 15:06:25 -0300

    quebra teste de proposito para testar CI

---
+++ src/test/java/com/estudos/funcionario/service/FuncionarioServiceTest.java
@@ -0,0 +1,34 @@
+package com.estudos.funcionario.service;
+
+import com.estudos.funcionario.entity.Funcionario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class FuncionarioServiceTest {
+
+    @Mock
+    private com.estudos.funcionario.repository.FuncionarioRepository repository;
+
+    @Mock
+    private DepartamentoService departamentoService;
+
+    @InjectMocks
+    private FuncionarioService service;
+
+    @Test
+    void deveCalcularMediaSalarial() {
+        Funcionario f1 = new Funcionario("Junio", 5000, null);
+        Funcionario f2 = new Funcionario("Maria", 7000, null);
+
+        double resultado = service.mediaSalarial(List.of(f1, f2));
+
+        assertEquals(6000, resultado);
+    }
+}
